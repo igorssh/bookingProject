743609388b311e6d1bc46a1c3522e93d6faa963a
Igor Shevchuk <igors.shevchuk@gmail.com>
2015-07-23 23:47:13 +0300

    Rebuilding sql dump file, fixing markup

--- database/databaseBookingCreation.sql
+++ database/databaseBookingCreation.sql
@@ -22,19 +22,20 @@ SET time_zone = "+00:00";
 
 -- --------------------------------------------------------
 
---
--- Table `apartaments`
---
-CREATE SCHEMA IF NOT EXISTS `BookingProject` DEFAULT CHARACTER SET utf8 ;
-USE `BookingProject` ;
+CREATE SCHEMA IF NOT EXISTS `bookingproject` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE `bookingproject` ;
 
+--
+-- Table `apartments`
+--
 
-CREATE TABLE IF NOT EXISTS `apartaments` (
-`id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `apartments` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `label` varchar(255) NOT NULL,
   `adress` varchar(255) NOT NULL,
-  `desc_text` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `desc_text` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -43,11 +44,31 @@ CREATE TABLE IF NOT EXISTS `apartaments` (
 --
 
 CREATE TABLE IF NOT EXISTS `apclasses` (
-`id` int(11) NOT NULL,
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `classId` tinyint(4) NOT NULL DEFAULT '1',
-  `desc_text` text CHARACTER SET utf32 NOT NULL,
-  `num_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `desc_text` text NOT NULL,
+  `num_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `num_id` (`num_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table `clients`
+--
+
+CREATE TABLE IF NOT EXISTS `clients` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `surname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `tele` varchar(100) NOT NULL,
+  `reg_num` varchar(100) NOT NULL,
+  `pers_num` varchar(100) NOT NULL,
+  `corp` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -56,12 +77,18 @@ CREATE TABLE IF NOT EXISTS `apclasses` (
 --
 
 CREATE TABLE IF NOT EXISTS `comments` (
-`id` int(11) unsigned NOT NULL,
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `head` varchar(255) NOT NULL,
   `desc_text` text NOT NULL,
   `time_stamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `klient_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `client_id` int(11) UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `client_id` (`client_id`),
+  CONSTRAINT `comments_ibfk_1`
+    FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -70,24 +97,27 @@ CREATE TABLE IF NOT EXISTS `comments` (
 --
 
 CREATE TABLE IF NOT EXISTS `extras` (
-`id` int(11) unsigned NOT NULL,
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `label` varchar(255) NOT NULL,
   `desc_text` text NOT NULL,
   `cost` double NOT NULL DEFAULT '0',
-  `pic` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `pic` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table `extra_rezervation_relations`
+-- Table `extra_reservation_relations`
 --
 
-CREATE TABLE IF NOT EXISTS `extra_rezervation_relations` (
-`id` int(11) NOT NULL,
-  `rez_id` int(11) NOT NULL,
-  `extra_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `extra_reservation_relations` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `res_id` int(11) NOT NULL,
+  `extra_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `res_id` (`res_id`, `extra_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -96,60 +126,33 @@ CREATE TABLE IF NOT EXISTS `extra_rezervation_relations` (
 --
 
 CREATE TABLE IF NOT EXISTS `functions` (
-`id` int(11) NOT NULL,
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `label` varchar(100) NOT NULL,
-  `desc_text` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `desc_text` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table `klients`
+-- Table `payments`
 --
 
-CREATE TABLE IF NOT EXISTS `klients` (
-`id` int(11) NOT NULL,
-  `name` varchar(255) CHARACTER SET utf32 NOT NULL,
-  `surname` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `tele` varchar(100) NOT NULL,
-  `reg_num` varchar(100) NOT NULL,
-  `pers_num` varchar(100) NOT NULL,
-  `corp` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table `paiments`
---
-
-CREATE TABLE IF NOT EXISTS `paiments` (
-`id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `payments` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `money` double NOT NULL DEFAULT '0',
   `desc_text` text NOT NULL,
   `pay_type` tinyint(4) NOT NULL DEFAULT '1',
   `time_stamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `referent` varchar(255) NOT NULL,
-  `klient_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table `rezervations`
---
-
-CREATE TABLE IF NOT EXISTS `rezervations` (
-`id` int(11) NOT NULL,
-  `from_date` date NOT NULL,
-  `to_date` date NOT NULL,
-  `p_count` int(11) NOT NULL,
-  `time_stamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `status` tinyint(1) NOT NULL DEFAULT '0',
-  `room_id` int(11) NOT NULL,
-  `klient_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `client_id` int(11) UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `client_id` (`client_id`),
+  CONSTRAINT `payments_ibfk_1`
+    FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -158,11 +161,17 @@ CREATE TABLE IF NOT EXISTS `rezervations` (
 --
 
 CREATE TABLE IF NOT EXISTS `roles` (
-`id` int(11) NOT NULL,
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `label` varchar(100) NOT NULL,
-  `desc_text` text CHARACTER SET utf8 COLLATE utf8_estonian_ci NOT NULL,
-  `func_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `desc_text` text NOT NULL,
+  `func_id` int(11) UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `func_id` (`func_id`),
+  CONSTRAINT `roles_ibfk_1`
+  FOREIGN KEY (`func_id`) REFERENCES `functions` (`id`)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -171,15 +180,54 @@ CREATE TABLE IF NOT EXISTS `roles` (
 --
 
 CREATE TABLE IF NOT EXISTS `rooms` (
-`id` int(11) NOT NULL,
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `num` int(11) NOT NULL,
   `p_count` int(11) NOT NULL,
   `price_per_day` double NOT NULL,
   `desc_text` text NOT NULL,
   `texn_repo` date NOT NULL,
-  `apclass_id` int(11) NOT NULL,
-  `ap_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `apclass_id` int(11) UNSIGNED NOT NULL,
+  `ap_id` int(11) UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `apclass_id` (`apclass_id`),
+  KEY `ap_id` (`ap_id`),
+  CONSTRAINT `rooms_ibfk_1`
+  FOREIGN KEY (`ap_id`) REFERENCES `apartments` (`id`)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+  CONSTRAINT `rooms_ibfk_2`
+  FOREIGN KEY (`apclass_id`) REFERENCES `apclasses` (`id`)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table `reservations`
+--
+
+CREATE TABLE IF NOT EXISTS `reservations` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `from_date` date NOT NULL,
+  `to_date` date NOT NULL,
+  `p_count` int(11) NOT NULL,
+  `time_stamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` tinyint(1) NOT NULL DEFAULT '0',
+  `room_id` int(11) UNSIGNED NOT NULL,
+  `client_id` int(11) UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `room_id` (`room_id`),
+  KEY `client_id` (`client_id`),
+  CONSTRAINT `reservations_ibfk_1`
+    FOREIGN KEY (`room_id`) REFERENCES `rooms` (`id`)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+  CONSTRAINT `reservations_ibfk_2`
+    FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -188,12 +236,18 @@ CREATE TABLE IF NOT EXISTS `rooms` (
 --
 
 CREATE TABLE IF NOT EXISTS `thumbs` (
-`id` int(11) NOT NULL,
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `label` varchar(255) NOT NULL,
   `desc_text` text NOT NULL,
   `orig` varchar(255) NOT NULL,
-  `room_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `room_id` int(11) UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `room_id` (`room_id`),
+  CONSTRAINT `thumbs_ibfk_1`
+    FOREIGN KEY (`room_id`) REFERENCES `rooms` (`id`)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -202,7 +256,7 @@ CREATE TABLE IF NOT EXISTS `thumbs` (
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
-`id` int(11) NOT NULL,
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `surname` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
@@ -210,207 +264,15 @@ CREATE TABLE IF NOT EXISTS `users` (
   `password` varchar(255) NOT NULL,
   `last_modify` date NOT NULL,
   `pub_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `role_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `role_id` int(11) UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `role_id` (`role_id`),
+  CONSTRAINT `users_ibfk_1`
+    FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_unicode_ci;
 
---
--- Indexes
---
-
---
--- Index `apartaments`
---
-ALTER TABLE `apartaments`
- ADD PRIMARY KEY (`id`);
-
---
--- Index `apclasses`
---
-ALTER TABLE `apclasses`
- ADD PRIMARY KEY (`id`), ADD KEY `num_id` (`num_id`);
-
---
--- Index `comments`
---
-ALTER TABLE `comments`
- ADD PRIMARY KEY (`id`), ADD KEY `klient_id` (`klient_id`);
-
---
--- Index `extras`
---
-ALTER TABLE `extras`
- ADD PRIMARY KEY (`id`);
-
---
--- Index `extra_rezervation_relations`
---
-ALTER TABLE `extra_rezervation_relations`
- ADD PRIMARY KEY (`id`), ADD KEY `rez_id` (`rez_id`,`extra_id`);
-
---
--- Index `functions`
---
-ALTER TABLE `functions`
- ADD PRIMARY KEY (`id`);
-
---
--- Index `klients`
---
-ALTER TABLE `klients`
- ADD PRIMARY KEY (`id`);
-
---
--- Index `paiments`
---
-ALTER TABLE `paiments`
- ADD PRIMARY KEY (`id`), ADD KEY `klient_id` (`klient_id`);
-
---
--- Index `rezervations`
---
-ALTER TABLE `rezervations`
- ADD PRIMARY KEY (`id`), ADD KEY `room_id` (`room_id`), ADD KEY `klient_id` (`klient_id`);
-
---
--- Index `roles`
---
-ALTER TABLE `roles`
- ADD PRIMARY KEY (`id`), ADD KEY `func_id` (`func_id`);
-
---
--- Index `rooms`
---
-ALTER TABLE `rooms`
- ADD PRIMARY KEY (`id`), ADD KEY `apclass_id` (`apclass_id`), ADD KEY `ap_id` (`ap_id`);
-
---
--- Index `thumbs`
---
-ALTER TABLE `thumbs`
- ADD PRIMARY KEY (`id`), ADD KEY `room_id` (`room_id`);
-
---
--- Index `users`
---
-ALTER TABLE `users`
- ADD PRIMARY KEY (`id`), ADD KEY `role_id` (`role_id`);
-
---
--- AUTO_INCREMENT 
---
-
---
--- AUTO_INCREMENT for `apartaments`
---
-ALTER TABLE `apartaments`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for `apclasses`
---
-ALTER TABLE `apclasses`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for `comments`
---
-ALTER TABLE `comments`
-MODIFY `id` int(11) unsigned NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for `extras`
---
-ALTER TABLE `extras`
-MODIFY `id` int(11) unsigned NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for `extra_rezervation_relations`
---
-ALTER TABLE `extra_rezervation_relations`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for `functions`
---
-ALTER TABLE `functions`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for `klients`
---
-ALTER TABLE `klients`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for `paiments`
---
-ALTER TABLE `paiments`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for `rezervations`
---
-ALTER TABLE `rezervations`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for `roles`
---
-ALTER TABLE `roles`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for `rooms`
---
-ALTER TABLE `rooms`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for `thumbs`
---
-ALTER TABLE `thumbs`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for `users`
---
-ALTER TABLE `users`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- Foreign key
---
-
---
--- Foreign key `comments`
---
-ALTER TABLE `comments`
-ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`klient_id`) REFERENCES `klients` (`id`);
-
---
--- Foreign key `paiments`
---
-ALTER TABLE `paiments`
-ADD CONSTRAINT `paiments_ibfk_1` FOREIGN KEY (`klient_id`) REFERENCES `klients` (`id`);
-
---
--- Foreign key `rezervations`
---
-ALTER TABLE `rezervations`
-ADD CONSTRAINT `rezervations_ibfk_1` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`id`),
-ADD CONSTRAINT `rezervations_ibfk_2` FOREIGN KEY (`klient_id`) REFERENCES `klients` (`id`);
-
---
--- Foreign key `roles`
---
-ALTER TABLE `roles`
-ADD CONSTRAINT `roles_ibfk_1` FOREIGN KEY (`func_id`) REFERENCES `functions` (`id`);
-
---
--- Foreign key `rooms`
---
-ALTER TABLE `rooms`
-ADD CONSTRAINT `rooms_ibfk_1` FOREIGN KEY (`ap_id`) REFERENCES `apartaments` (`id`),
-ADD CONSTRAINT `rooms_ibfk_2` FOREIGN KEY (`apclass_id`) REFERENCES `apclasses` (`id`);
-
---
--- Foreign key `thumbs`
---
-ALTER TABLE `thumbs`
-ADD CONSTRAINT `thumbs_ibfk_1` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`id`);
-
---
--- Foreign key `users`
---
-ALTER TABLE `users`
-ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
