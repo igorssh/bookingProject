5e84cad1ee5f68cb8956bc77b9dde9ba7ed6954f
Andrejs Cunskis <andrejs.cunskis@4finance.com>
2015-09-13 22:54:11 +0300

    Small tests fix so that DB is not filled up

--- src/test/java/lv/javaguru/java2/core/database/jdbc/frontend/SearchDAOImplTest.java
+++ src/test/java/lv/javaguru/java2/core/database/jdbc/frontend/SearchDAOImplTest.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -23,6 +24,7 @@ import static org.junit.Assert.assertTrue;
 @ContextConfiguration(classes = SpringConfig.class)
 @WebAppConfiguration
 
+@Transactional
 public class SearchDAOImplTest {
 
     @Autowired
